a6b3e6cb3467eaf12fbc7827f4419a0d92d5a343
Shubhankar Abhay Kamthankar <80084594+So-bonkers@users.noreply.github.com>
2023-05-16 00:46:37 +0530

    Add files via upload

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pattern (int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = N-1; i >0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int N;
+    cout << "Pattern 10: Half Diamond Star Pattern \nEnter the value of N: ";
+    cin >> N;
+    pattern(N);
+    return 0;
+}
